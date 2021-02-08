155322ed3bcb4398302e0ac35cdbf7c3c9ef1565
Damon Bogich <damonbogich@gmail.com>
2021-02-08 13:43:05 -0700

    styling for completed todo done

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
 
 export default function Todo({dispatch, item}) {
     return (
-        <p id={item.id} onClick={(e) => dispatch({type: 'TOGGLE_TODO', payload: e.target.id})}>{item.item}</p>
+        <p style={{textDecoration: item.completed ? 'line-through' : 'none' }} id={item.id} onClick={(e) => dispatch({type: 'TOGGLE_TODO', payload: e.target.id})}>{item.item}</p>
     )
 }
